9747098c02831efb03ed416e80fb79b3f12370c4
zillyf <zilly@adminc.org>
2022-10-09 18:27:44 +0200

    initial version

---
+++ datasetmanager/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '1.1'
+
+
+def hello_world():
+    print("Hello World, Dataset Manager!")
+
+def create_dataset(name):
+    import os
+
+def add_image(filename):
+    # check MD5 write MD5
+    return image_hash
+
+def add_image_meta(namespace, json):
+    # create json
+    pass
+
+def get_item():
+    pass
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+from datasetmanager import __version__
+
+packages=find_packages()
